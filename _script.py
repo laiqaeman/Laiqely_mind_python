840379d547ee84f3d9a8091b43d9f6c83ad76072
laiqaeman <laiqaeman1011@gmail.com>
2025-02-24 15:54:09 +0500

    Removed unused seaborn import

--- _script.py
+++ _script.py
@@ -15,7 +15,6 @@ import pandas as pd
 import plotly.express as px
 import pyaudio
 import pytesseract
-import seaborn as sns
 import speech_recognition as sr
 import streamlit as st
 from fpdf import FPDF
